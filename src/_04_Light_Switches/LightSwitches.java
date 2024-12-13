3d572e54f150f8270b64aa60bdd0edf680870320
Evan Keri <Evan Keri@DESKTOP-2O56U0E.sdcs.local>
2024-12-13 14:20:40 -0800

    finished the code but the tester seems broken. fix?

--- src/_04_Light_Switches/LightSwitches.java
+++ src/_04_Light_Switches/LightSwitches.java
@@ -55,7 +55,9 @@ public class LightSwitches implements GameControlScene {
      * index = 6        // return true if pink is on (bit 6 == 1)
      */
     boolean isLightOn(int index) {
-        return false;
+    	int e = (int)Math.pow(2, index);
+    	int comp = e&lightsOnOff;
+        return comp==e;
     }
     
     /*
@@ -63,7 +65,9 @@ public class LightSwitches implements GameControlScene {
      * index = 4        // turn off yellow only (set bit 4 = 1)
      */
     void turnLightOn(int index) {
-        
+        if(!isLightOn(index)) {
+        	lightsOnOff+=Math.pow(2, index);
+        }
     }
     
     /*
@@ -71,7 +75,9 @@ public class LightSwitches implements GameControlScene {
      * index = 0        // turn off blue only (set bit 0 = 0)
      */
     void turnLightOff(int index) {
-        
+    	if(isLightOn(index)) {
+        	lightsOnOff-=Math.pow(2, index);
+        }
     }
     
     /*
@@ -79,7 +85,12 @@ public class LightSwitches implements GameControlScene {
      * lightsBitmap = 0b01100110  // lights 1, 2, 5, 6 on
      */
     void turnMultiLightsOn(int lightsBitmap) {
-        
+        for(int i = 1;i<8;i++) {
+        	int e = (int) Math.pow(2, i);
+        	if((lightsBitmap&e)==e) {
+        		turnLightOn(i);
+        	}
+        }
     }
     
     /*
@@ -87,7 +98,12 @@ public class LightSwitches implements GameControlScene {
      * lightsBitmap = 0b10000001  // lights 0, 7 off
      */
     void turnMultiLightsOff(int lightsBitmap) {
-        
+    	for(int i = 1;i<8;i++) {
+        	int e = (int) Math.pow(2, i);
+        	if((lightsBitmap&e)==e) {
+        		turnLightOff(i);
+        	}
+        }
     }
     
     /*
@@ -100,7 +116,17 @@ public class LightSwitches implements GameControlScene {
      *                               orange(3) and yellow(4) on
      */
     void toggleLights(int lightsBitmap) {
-        
+    	for(int i = 1;i<8;i++) {
+        	int e = (int) Math.pow(2, i);
+        	if((lightsBitmap&e)==e) {
+        		//if its on turn off, if off turn on
+        		if((lightsOnOff&e)==e) {
+        			turnLightOff(i);
+        		} else {
+        			turnLightOn(i);
+        		}
+        	}
+        }
     }
     
     void runLightSequence1() {
--- src/_05_Base64_Decoder/Base64Decoder.java
+++ src/_05_Base64_Decoder/Base64Decoder.java
@@ -1,5 +1,7 @@
 package _05_Base64_Decoder;
 
+import java.util.ArrayList;
+
 /*
  * Base 64 is a way of encoding binary data using text.
  * Each number 0-63 is mapped to a character.
@@ -56,19 +58,78 @@ public class Base64Decoder {
     //1. Complete this method so that it returns the index in
     //   the base64Chars array that corresponds to the passed in char.
     public static byte convertBase64Char(char c){
-        return 0;
+    	for(byte b = 0;b<64;b++) {
+    		if(base64Chars[b]==c) {
+    			return b;
+    		}
+    	}
+    	return -1;
     }
 
     //2. Complete this method so that it will take in a string that is 4
     //   characters long and return an array of 3 bytes (24 bits). The byte
     //   array should be the binary value of the encoded characters.
     public static byte[] convert4CharsTo24Bits(String s){
-        return null;
+    	byte[] cb = new byte[4];
+    	byte[] bs = new byte[3];
+    	
+    	for(int i = 0; i<4;i++) {
+    		cb[i]=convertBase64Char(s.charAt(i));
+    	}
+    	
+    	bs[0] = (byte) (cb[0]<<2+cb[1]>>4); //last 6 of cb[0] and first 2 of cb[1]
+    	bs[1] = (byte) (cb[1]<<4+cb[2]>>2); //last 4 of cb[1] and first 4 of cb[2]
+    	bs[2] = (byte) (cb[2]<<4+cb[3]); //last 2 of cb[2] and first 6 of cb[3]
+    	return bs;
     }
 
     //3. Complete this method so that it takes in a string of any length
     //   and returns the full byte array of the decoded base64 characters.
     public static byte[] base64StringToByteArray(String file) {
-        return null;
+    	ArrayList<Byte> list = new ArrayList<Byte>();
+    	byte[] ray;
+    	while(file.length()>=4) {
+    		for(byte b: convert4CharsTo24Bits(file.substring(0, 4))) {
+    			list.add(b);
+    		}
+    		file=file.substring(4);
+    	}
+    	if(file.length()>0) {
+    		//convert the last 1-3 chars to bytes
+    		//problem: chars store 6 bits and bytes store 8
+    		//1 char: 1 byte, 2 unused
+    		//2 char: 2 byte, 4 unused
+    		//3 char: 3 byte 6 unused
+    		//add bytes to list
+    		byte b;
+    		byte[] bs = new byte[file.length()];
+    		char[] cs = file.toCharArray();
+    		for(int i = 0;i<file.length();i++) {
+    			switch (i) {
+    			case 0:
+    				bs[0]=(byte) (convertBase64Char(cs[0])*4);
+    				break;
+    			case 1:
+    				b = convertBase64Char(cs[1]);
+    				bs[0]+=(b>>4);
+    				bs[1]=(byte) (b<<4);
+    				break;
+    			case 2:
+    				b = convertBase64Char(cs[1]);
+    				bs[1]+=(b>>2);
+    				bs[2]=(byte) (b<<6);
+    				break;
+    			}
+    		}
+    		for(byte be: bs) {
+    			list.add(be);
+    		}
+    	} 
+    	ray=new byte[list.size()];
+    	for(int i = 0; i<ray.length;i++) {
+    		ray[i]=list.get(i).byteValue();
+    	}
+    	
+        return ray;
     }
 }
